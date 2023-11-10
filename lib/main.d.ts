19b196043cfd0819dd77d2f4313ebabeaf2b4f9d
AntoinePollet <pollet.antoine.alexis@gmail.com>
2023-11-10 18:07:10 +0100

    main.d.ts

---
+++ lib/main.d.ts
@@ -0,0 +1,13 @@
+declare module "./main.ts" {
+  import Checkbox from "./components/form/Checkbox.vue";
+  import FileInput from "./components/form/FileInput.vue";
+  import Input from "./components/form/Input.vue";
+  import Switch from "./components/form/Switch.vue";
+  import Dialog from "./components/Dialog.vue";
+  import Listbox from "./components/Listbox.vue";
+  import Popover from "./components/Popover.vue";
+  import Menu from "./components/menu/Menu.vue";
+  import MenuItem from "./components/menu/MenuItem.vue";
+
+  export { Checkbox, FileInput, Input, Switch, Dialog, Listbox, Popover, Menu, MenuItem };
+}
